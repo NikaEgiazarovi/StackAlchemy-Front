c94d75642218c16584056a5618417a625fb7f56e
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-02-27 09:58:07 +0400

    changed app-root styles.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: false,
-  
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
