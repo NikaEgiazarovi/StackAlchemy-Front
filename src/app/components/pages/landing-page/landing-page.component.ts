98acc6da4c7ebf73d3373162f1508d5a8d34dadc
Nika Egiazarovi <151972349+NikaEgiazarovi@users.noreply.github.com>
2025-03-15 22:31:49 +0400

    added-standalone

--- src/app/components/pages/landing-page/landing-page.component.ts
+++ src/app/components/pages/landing-page/landing-page.component.ts
@@ -7,6 +7,7 @@ import {
   animate,
 } from '@angular/animations';
 @Component({
+  standalone: false,
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
