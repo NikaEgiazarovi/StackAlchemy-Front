bac2828a401cd922a5fc8f9d3859efdfdbbcdf0b
Giorgi Gigauri <gigaurigiorgi2727@gmail.com>
2025-03-15 22:26:31 +0400

    fixed standalone issue.

--- src/app/components/pages/landing-page/landing-page.component.ts
+++ src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
