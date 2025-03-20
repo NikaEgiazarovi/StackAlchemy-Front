7fb9253e56dffa18269e0b22c63ba4c107276f1a
Nika Egiazarovi <151972349+NikaEgiazarovi@users.noreply.github.com>
2025-03-21 02:41:44 +0400

    registration-card-validations-added

--- src/app/components/pages/registration/registration.component.ts
+++ src/app/components/pages/registration/registration.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registration',
-  standalone: false,
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  regexUserName = /^[a-zA-Z]+$/;
+  regexGmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
 
+  handleLogin(event: Event) {
+    event.preventDefault();
+
+    const userNameInput = (document.getElementById("usermainName") as HTMLInputElement)?.value;
+    const userEmailInput = (document.getElementById("userGmail") as HTMLInputElement)?.value;
+    const userPasswordInput = (document.getElementById("userPassword") as HTMLInputElement)?.value;
+
+    if (!this.regexUserName.test(userNameInput)) {
+      Swal.fire("Oops...", "There are some numbers in your name!", "error");
+    } else if (!this.regexGmail.test(userEmailInput)) {
+      Swal.fire("Oops...", "Invalid email address!", "error");
+    } else if (!this.regexPassword.test(userPasswordInput)) {
+      Swal.fire("Oops...", "Invalid password!", "error");
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: true,
+        timer: 1500
+      }).then(() => {
+        window.location.href = "./Homepage.html";
+      });
+    }
+  }
 }
